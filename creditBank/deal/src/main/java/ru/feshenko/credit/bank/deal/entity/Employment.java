e845c6e50ef4387626f7d69325c4eb448bdc44b9
artyfeshenko <artyfeshenko@yandex.ru>
2026-04-06 21:34:55 +0300

    fix imports

--- creditBank/deal/src/main/java/ru/feshenko/credit/bank/deal/entity/Employment.java
+++ creditBank/deal/src/main/java/ru/feshenko/credit/bank/deal/entity/Employment.java
@@ -1,6 +1,13 @@
 package ru.feshenko.credit.bank.deal.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- creditBank/deal/src/main/java/ru/feshenko/credit/bank/deal/entity/Statement.java
+++ creditBank/deal/src/main/java/ru/feshenko/credit/bank/deal/entity/Statement.java
@@ -1,6 +1,16 @@
 package ru.feshenko.credit.bank.deal.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
